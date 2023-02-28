73e7f2248d754db9ae843f2fb628365696bd6347
sabitvrustam <sabitvrustam@gmail.com>
2023-02-28 07:04:48 +0300

    выполнение задания

---
+++ 1/1.1/string conversion.js
@@ -0,0 +1,14 @@
+function srtConv(str) {
+    let newStr = "";
+    for (i = 0; i <= str.length - 1; i++) {
+        if (i === 0) {
+            newStr += str[i].toUpperCase();
+        } else if (i !== 0 && str[i - 1] == " ") {
+            newStr += str[i].toUpperCase();
+        } else {
+            newStr += str[i].toLowerCase();
+        }
+    }
+    return newStr;
+}
+console.log(srtConv('привет кАк делА heLLOO wORLD'));
