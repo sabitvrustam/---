7e9c6a3c11b4b17ba879ab34807e5ee717f33993
sabitvrustam <sabitvrustam@gmail.com>
2023-02-28 20:40:39 +0300

    выполнение задания

---
+++ 3/class.js
@@ -0,0 +1,17 @@
+class Product {
+    constructor(name, price, count, description) {
+        this.name = name;
+        this.price = price;
+        this.count = count;
+        this.description = description;
+    }
+}
+
+let apple = new Product("яблоки", "450", "56", "очень вкусные");
+let tv = new Product("телевизор", "45000", "100", "led подсветка");
+
+
+
+
+
+console.log(apple/n, tv);
