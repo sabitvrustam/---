a1a4a622b15ac2e6b92142f31f97b96d9b93e58a
sabitvrustam <sabitvrustam@gmail.com>
2023-03-03 07:40:25 +0300

    выполнение задания

--- 1/1.4/Counting, unique words.js
+++ 1/1.4/Counting, unique words.js
@@ -1,21 +1,16 @@
-function countUniqueWorld (str) {
-    let mapStr = [];
-    let string = "";
-    let count = str.length - 1;
-    for (i = 0; i <= count ; i++) {
-        if (str[i] !== " " && str[i] != ","){
-           string += str[i]; 
-           if (i == count){
-            mapStr.push(string);
-        }
-        } else if (str[i] == ","){
-        } else {
-            mapStr.push(string);
-            string = "";
-        }
+
+
+function countUniqueWords(text) {
+    const words = text.split(/\W+/);
+    const counts = {};
+    for (let word of words) {
+      if (word in counts) {
+        counts[word]++;
+      } else {
+        counts[word] = 1;
+      }
     }
-   for (j =0; j <= mapStr.length - 1; j++){
-        console.log(mapStr[j]);
-   }
-}
-countUniqueWorld("Текст, в котором слово текст несколько раз встречается и слово тоже");
+    return counts;
+  }
+  
+  console.log(countUniqueWords("Текст, в котором слово текст несколько раз встречается и слово тоже"));
--- 3/class.js
+++ 3/class.js
@@ -5,13 +5,80 @@ class Product {
         this.count = count;
         this.description = description;
     }
+    matches(conditions) {
+      const parts = conditions.split("&");
+      for (let part of parts) {
+        const [key, operator, value] = part.split("-");
+        if (key === "name") {
+          if (operator === "contains" && !this.name.includes(value)) {
+            return false;
+          } else if (operator === "starts" && !this.name.startsWith(value)) {
+            return false;
+          } else if (operator === "ends" && !this.name.endsWith(value)) {
+            return false;
+          }
+        } else if (key === "price") {
+          if (operator === "<" && !(this.price < parseFloat(value))) {
+            return false;
+          } else if (operator === "<=" && !(this.price <= parseFloat(value))) {
+            return false;
+          } else if (operator === "=" && !(this.price === parseFloat(value))) {
+            return false;
+          } else if (operator === ">=" && !(this.price >= parseFloat(value))) {
+            return false;
+          } else if (operator === ">" && !(this.price > parseFloat(value))) {
+            return false;
+          }
+        } else if (key === "quantity") {
+          if (operator === "<" && !(this.quantity < parseInt(value))) {
+            return false;
+          } else if (operator === "<=" && !(this.quantity <= parseInt(value))) {
+            return false;
+          } else if (operator === "=" && !(this.quantity === parseInt(value))) {
+            return false;
+          } else if (operator === ">=" && !(this.quantity >= parseInt(value))) {
+            return false;
+          } else if (operator === ">" && !(this.quantity > parseInt(value))) {
+            return false;
+          }
+        } else if (key === "description") {
+          if (operator === "contains" && !this.description.includes(value)) {
+            return false;
+          } else if (operator === "starts" && !this.description.startsWith(value)) {
+            return false;
+          } else if (operator === "ends" && !this.description.endsWith(value)) {
+            return false;
+          }
+        }
+      }
+      
+      return this;
+      
+    }
 }
 
-let apple = new Product("яблоки", "450", "56", "очень вкусные");
-let tv = new Product("телевизор", "45000", "100", "led подсветка");
+let string = "name-contains-fd&price-=2&quantity->5&description-ends-abc";
+const product = [
+new Product("яблоки", 2, 56, "очень вкусные"),
+new Product("телевизор", 45000, 100, "led подсветка"),
+new Product("Товар 1", 10.99, 5, "Описание товара 1"),
+new Product("fd", 2, 8, "ghhabc"),
+new Product("fd", 2, 8, "ghhabc"),
+new Product("fd", 2, 8, "ghhabc"),
+
+];
+
+for (i = 0; i <= product.length - 1; i++){
+  let mach = [];
+  mach = (product[i].matches(string));
+  console.log(mach);
+}
 
 
 
 
 
-console.log(apple, tv);
+
+
+
+
